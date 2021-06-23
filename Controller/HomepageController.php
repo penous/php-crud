037c80e4057ff1fc77d72853a4f427770530ff46
Tom Dirinck <tom.dirinck@gmail.com>
2021-06-23 17:48:20 +0200

    Added post logic for creating new student.

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -6,17 +6,59 @@ class HomepageController {
     public function render(array $GET, array $POST) {
         $pdo = Connection::Open();
         $studentLoader = new StudenLoader($pdo);
+        $classLoader = new KlassLoader($pdo);
 
-        if ((isset($GET['page']) && $GET['page'] == 'students') || empty($GET)) {
+        function loadStudents($studentLoader) {
             $studentsData = $studentLoader->getAllStudents();
             foreach ($studentsData as  $student) {
                 $student = $studentLoader->createStudent($student);
                 $students[] = $student;
             }
-            var_dump($student);
+            return $students;
+        }
+
+        // ADD student TO DB
+        if ((isset($POST['object']) && $POST['object'] == 'student')) {
+            $GET = [];
+            $GET['post'] = 'post';
+            array_splice($POST, -2);
+            $studentLoader->addStudent($POST);
+
+            // Load student AGAIN
+            $students = loadStudents($studentLoader);
 
             //load the view
             require 'View/studentIndex.php';
+        }
+
+        // INDEX PAGE students
+        if ((isset($GET['page']) && $GET['page'] == 'students') || empty($GET)) {
+            // Load student
+            $students = loadStudents($studentLoader);
+
+            //load the view
+            require 'View/studentIndex.php';
+        }
+
+        // DETAILS PAGE student
+        if (isset($GET['button']) && $GET['button'] == 'Details') {
+            $student = $studentLoader->GetStudentById($GET['id']);
+            $student = $studentLoader->createStudent($student);
+
+            //load the view
+            require 'View/studentDetails.php';
+        }
+
+        // CREATE NEW student PAGE
+        if (isset($GET['button']) && $GET['button'] == 'New') {
+            $classData = $classLoader->getAllClasses();
+            foreach ($classData as  $class) {
+                $class = $classLoader->createClass($class);
+                $classes[] = $class;
+            }
+
+            //load the view
+            require 'View/studentNew.php';
         }
     }
 }
